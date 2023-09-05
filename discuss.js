ccbd73f2b400b4732e3707ca47b9d559a74b8c14
jatinmishra1 <balajimahraaj123@gmail.com>
2023-09-05 18:45:57 +0530

    first commit

---
+++ discuss.js
@@ -0,0 +1,42 @@
+// Requirement Clarification
+//Features
+//Tech Stack
+    //  --Redux
+    //  --Tailwid CSS
+    //supoose you are using  form  then tell or ask them i am going to use this library for forms
+    // i am going to use react-router-dom fot routing
+    // you can tell you that what bundler you are choosing
+    //you talk about what testing library you are choosing
+    //   Spent max-to-max 5 min on this
+
+
+    // do the low level design  i.e ui design ,how data fill flow,how you will structre your components
+    // so builting youtube
+    //so we will have header  logo          seacr bar      user icon
+    //side panel          body
+    //                   buttons and videos
+    //                  click on video get to that video ,that is it will tak to us new page
+    // page with comment section ,same header,subscribe and many more things
+    // you have to discuss this in 5 minutes
+
+    // so we will divided in to components --- sidepanel compoennt----video-container-component---video var component---
+    //---header component
+    // watch video component
+
+
+    // now will tell that i am using create-react-app
+    
+
+
+    import react from 'react'
+
+const Head=()=>{
+    return (
+        <>
+        </>
+    )
+}
+export default Head
+
+
+//Learn grid system for css
